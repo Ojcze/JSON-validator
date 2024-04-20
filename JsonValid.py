935daaa9355d5891dcff5c3a38457d40d4d99c89
Ojcze <84791578+Ojcze@users.noreply.github.com>
2024-04-20 17:27:05 +0200

    run from cmd
    adding the ability to run from the cmd (and choose checking function)

--- JsonValid.py
+++ JsonValid.py
@@ -1,18 +1,9 @@
 import json
 import re
+import sys
+import os
 
-
-# JSON validator by check regex (default)
-def json_resource_validator_regex(path: str) -> bool:
-    with open(path, 'r') as file:
-        data = file.read()
-
-    if re.search(r'\s*"Resource"\s*:\s*"\*"\s*', data):
-        return False
-    return True
-
-
-# JSON validator by check objects
+# JSON validator by check objects (default)
 def json_resource_validator_obj(path: str) -> bool:
     with open(path, 'r') as file:
         data = json.load(file)
@@ -29,3 +20,31 @@ def json_resource_validator_obj(path: str) -> bool:
                         if state['Resource'] == '*':
                             return False
     return True
+
+# JSON validator by check regex
+def json_resource_validator_regex(path: str) -> bool:
+    with open(path, 'r') as file:
+        data = file.read()
+
+    if re.search(r'\s*"Resource"\s*:\s*"\*"\s*', data):
+        return False
+    return True
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print("No file. Use command with argument like ~", sys.argv[0], "<path/to/file.json>")
+        sys.exit(0)
+    if not os.path.isfile(sys.argv[1]):
+        print(sys.argv[1],"is not a file!")
+        sys.exit(0)
+
+    if len(sys.argv) >= 3 and sys.argv[2] == "--regex":
+        func = json_resource_validator_regex
+    else:
+        func = json_resource_validator_obj
+
+    if func(sys.argv[1]):
+        print("File is valid")
+    else:
+        print("File is invalid!")
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # JSON-validator
 Project created for recruitment purposes
+
+## How to use
+To validate a file JSON use a script as below
+
+~python <path>/JsonValid.py <path/to/json/file>
+
+## Additional information
+The script is not fully "idiot-proof"
+- File JsonValid.py contains two different functions to check AWS::IAM::Role Policy (validation is according to a specific criterion):
+1. by objects structure,
+2. by regex.
+
+The JSON validator via object checking is the default.
+This is due to one unhandled case yet (I'm not sure if it's even possible)
+
+-The "Resource" field contains a list, but one of the list items is "*"-
+
+regular expression functions need to be improved for this case.
+You can still use regex option if you add flag "--regex" as next argument in cmd.
+For example: ~python /JsonValid.py <path/to/json/file> --regex
