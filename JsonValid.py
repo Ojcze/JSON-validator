dd7ad1a17c8eb5f32235ef88efe0e3e31047b873
Ojcze <84791578+Ojcze@users.noreply.github.com>
2024-04-20 16:20:27 +0200

    resource list handle
    json_resource_validator_obj - handling the case when the "Resource" field is a list

--- JsonValid.py
+++ JsonValid.py
@@ -2,8 +2,8 @@ import json
 import re
 
 
-# JSON validator by check regex
-def jason_aws_validator_regex(path: str) -> bool:
+# JSON validator by check regex (default)
+def json_resource_validator_regex(path: str) -> bool:
     with open(path, 'r') as file:
         data = file.read()
 
@@ -13,7 +13,7 @@ def jason_aws_validator_regex(path: str) -> bool:
 
 
 # JSON validator by check objects
-def jason_aws_validator_obj(path: str) -> bool:
+def json_resource_validator_obj(path: str) -> bool:
     with open(path, 'r') as file:
         data = json.load(file)
 
@@ -21,6 +21,11 @@ def jason_aws_validator_obj(path: str) -> bool:
         if 'Statement' in data['PolicyDocument']:
             for state in data['PolicyDocument']['Statement']:
                 if 'Resource' in state:
-                    if state['Resource'] == '*':
-                        return False
+                    if isinstance(state['Resource'], list):
+                        for res in state['Resource']:
+                            if res == '*':
+                                return False
+                    else:
+                        if state['Resource'] == '*':
+                            return False
     return True
